8e4a1a14022d40da1701829b3b23888fea3fc3f8
artslavc <artslavc@gmail.com>
2026-06-18 15:20:39 +1000

    Services, models

---
+++ Backend/AdminAPI/Models/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace AdminAPI.Models
+{
+    public class LoginRequest
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Backend/AdminAPI/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace AdminAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string Role { get; set; } = "user";
+    }
+}
